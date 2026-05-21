b85ba0bf4359bcb08fcf1ca6574beda11dfb5d33
Nerd004 <azaz11197644@gmail.com>
2026-05-21 12:30:36 +0400

    Initial setup

---
+++ GoldApp/GoldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoldAppApp.swift
+//  GoldApp
+//
+//  Created by حسين الحمري on 21/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct GoldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoldAppTests/GoldAppTests.swift
@@ -0,0 +1,18 @@
+//
+//  GoldAppTests.swift
+//  GoldAppTests
+//
+//  Created by حسين الحمري on 21/05/2026.
+//
+
+import Testing
+
+struct GoldAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
